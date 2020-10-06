e37dae9b267d94e81c77f651fb5097a6e666806f
kyweez <auboudier.pro@gmail.com>
2020-10-06 07:34:56 +0200

    Coffee time - Controles

--- EXERCISES-CDA/INPUT-VALIDATION/Controles.Designer.cs
+++ EXERCISES-CDA/INPUT-VALIDATION/Controles.Designer.cs
@@ -28,14 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Controles));
             this.labelName = new System.Windows.Forms.Label();
             this.labelDate = new System.Windows.Forms.Label();
             this.labelAmount = new System.Windows.Forms.Label();
             this.labelZipCode = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textName = new System.Windows.Forms.TextBox();
+            this.textZipCode = new System.Windows.Forms.TextBox();
+            this.textAmount = new System.Windows.Forms.TextBox();
+            this.textDate = new System.Windows.Forms.TextBox();
             this.labelDateTip = new System.Windows.Forms.Label();
             this.buttonValidate = new System.Windows.Forms.Button();
             this.buttonReset = new System.Windows.Forms.Button();
@@ -43,114 +44,81 @@
             // 
             // labelName
             // 
-            this.labelName.AutoSize = true;
-            this.labelName.Location = new System.Drawing.Point(25, 34);
+            resources.ApplyResources(this.labelName, "labelName");
             this.labelName.Name = "labelName";
-            this.labelName.Size = new System.Drawing.Size(29, 13);
-            this.labelName.TabIndex = 0;
-            this.labelName.Text = "Nom";
             // 
             // labelDate
             // 
-            this.labelDate.AutoSize = true;
-            this.labelDate.Location = new System.Drawing.Point(25, 74);
+            resources.ApplyResources(this.labelDate, "labelDate");
             this.labelDate.Name = "labelDate";
-            this.labelDate.Size = new System.Drawing.Size(30, 13);
-            this.labelDate.TabIndex = 1;
-            this.labelDate.Text = "Date";
             // 
             // labelAmount
             // 
-            this.labelAmount.AutoSize = true;
-            this.labelAmount.Location = new System.Drawing.Point(25, 114);
+            resources.ApplyResources(this.labelAmount, "labelAmount");
             this.labelAmount.Name = "labelAmount";
-            this.labelAmount.Size = new System.Drawing.Size(46, 13);
-            this.labelAmount.TabIndex = 2;
-            this.labelAmount.Text = "Montant";
             // 
             // labelZipCode
             // 
-            this.labelZipCode.AutoSize = true;
-            this.labelZipCode.Location = new System.Drawing.Point(25, 154);
+            resources.ApplyResources(this.labelZipCode, "labelZipCode");
             this.labelZipCode.Name = "labelZipCode";
-            this.labelZipCode.Size = new System.Drawing.Size(64, 13);
-            this.labelZipCode.TabIndex = 3;
-            this.labelZipCode.Text = "Code Postal";
             // 
-            // textBox1
+            // textName
             // 
-            this.textBox1.Location = new System.Drawing.Point(109, 27);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(168, 20);
-            this.textBox1.TabIndex = 4;
+            resources.ApplyResources(this.textName, "textName");
+            this.textName.Name = "textName";
+            this.textName.Validating += new System.ComponentModel.CancelEventHandler(this.textName_Validating);
             // 
-            // textBox2
+            // textZipCode
             // 
-            this.textBox2.Location = new System.Drawing.Point(109, 147);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(39, 20);
-            this.textBox2.TabIndex = 5;
+            resources.ApplyResources(this.textZipCode, "textZipCode");
+            this.textZipCode.Name = "textZipCode";
             // 
-            // textBox3
+            // textAmount
             // 
-            this.textBox3.Location = new System.Drawing.Point(109, 107);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(91, 20);
-            this.textBox3.TabIndex = 6;
+            resources.ApplyResources(this.textAmount, "textAmount");
+            this.textAmount.Name = "textAmount";
             // 
-            // textBox4
+            // textDate
             // 
-            this.textBox4.Location = new System.Drawing.Point(109, 67);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(91, 20);
-            this.textBox4.TabIndex = 7;
+            resources.ApplyResources(this.textDate, "textDate");
+            this.textDate.Name = "textDate";
             // 
             // labelDateTip
             // 
-            this.labelDateTip.AutoSize = true;
-            this.labelDateTip.Location = new System.Drawing.Point(227, 74);
+            resources.ApplyResources(this.labelDateTip, "labelDateTip");
             this.labelDateTip.Name = "labelDateTip";
-            this.labelDateTip.Size = new System.Drawing.Size(79, 13);
-            this.labelDateTip.TabIndex = 8;
-            this.labelDateTip.Text = "(JJ/MM/AAAA)";
             // 
             // buttonValidate
             // 
-            this.buttonValidate.Location = new System.Drawing.Point(262, 154);
+            resources.ApplyResources(this.buttonValidate, "buttonValidate");
             this.buttonValidate.Name = "buttonValidate";
-            this.buttonValidate.Size = new System.Drawing.Size(75, 23);
-            this.buttonValidate.TabIndex = 9;
-            this.buttonValidate.Text = "Valider";
             this.buttonValidate.UseVisualStyleBackColor = true;
+            this.buttonValidate.Click += new System.EventHandler(this.buttonValidate_Click);
             // 
             // buttonReset
             // 
-            this.buttonReset.Location = new System.Drawing.Point(262, 183);
+            resources.ApplyResources(this.buttonReset, "buttonReset");
             this.buttonReset.Name = "buttonReset";
-            this.buttonReset.Size = new System.Drawing.Size(75, 23);
-            this.buttonReset.TabIndex = 10;
-            this.buttonReset.Text = "Effacer";
             this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
             // 
             // Controles
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(365, 224);
             this.Controls.Add(this.buttonReset);
             this.Controls.Add(this.buttonValidate);
             this.Controls.Add(this.labelDateTip);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textDate);
+            this.Controls.Add(this.textAmount);
+            this.Controls.Add(this.textZipCode);
+            this.Controls.Add(this.textName);
             this.Controls.Add(this.labelZipCode);
             this.Controls.Add(this.labelAmount);
             this.Controls.Add(this.labelDate);
             this.Controls.Add(this.labelName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Name = "Controles";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Les contrôles";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -162,10 +130,10 @@
         private System.Windows.Forms.Label labelDate;
         private System.Windows.Forms.Label labelAmount;
         private System.Windows.Forms.Label labelZipCode;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textName;
+        private System.Windows.Forms.TextBox textZipCode;
+        private System.Windows.Forms.TextBox textAmount;
+        private System.Windows.Forms.TextBox textDate;
         private System.Windows.Forms.Label labelDateTip;
         private System.Windows.Forms.Button buttonValidate;
         private System.Windows.Forms.Button buttonReset;
--- EXERCISES-CDA/INPUT-VALIDATION/Controles.cs
+++ EXERCISES-CDA/INPUT-VALIDATION/Controles.cs
@@ -12,9 +12,42 @@ namespace INPUT_VALIDATION
 {
     public partial class Controles : Form
     {
+        public string name;
+        public DateTime date;
+        public float amount;
+        public string zipcode;
+
         public Controles()
         {
             InitializeComponent();
+        }
+
+        private void textName_Validating(object sender, CancelEventArgs e)
+        {
+            if(string.IsNullOrWhiteSpace(textName.Text))
+            {
+                e.Cancel = true;
+                textName.Focus();
+                ErrorProvider.Equals(textName, "Please insert your name");
+            }
+            else
+            {
+                e.Cancel = false;
+                ErrorProvider.Equals(textName, null);
+            }
+        }
+
+        private void buttonValidate_Click(object sender, EventArgs e)
+        {
+            if (ValidateChildren(ValidationConstraints.Enabled))
+            {
+                MessageBox.Show("Coucou", "");
+            }
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+
         }
 
     }
