24bf749903f09083f7a82860a7c467e5a4215321
kyweez <auboudier.pro@gmail.com>
2020-10-08 22:23:27 +0200

    Refactoring tests

--- EXERCISES-CDA/ZOO/Program.cs
+++ EXERCISES-CDA/ZOO/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using ZOO.ClassTest;
+﻿using ZOO.ClassTest;
 
 namespace ZOO
 {
@@ -7,61 +6,11 @@ namespace ZOO
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("TEST 1 : Guards initialization");
-            Console.WriteLine("______________________________\n");
-            TestGuard.InitializingGuards();
-            Console.WriteLine("Press any key to continue...");
-            Console.ReadKey();
-            Console.Clear();
+            /*##############################################*/
+            /*## UNCOMMENT THE DESIRED TEST TO LAUCNCH IT ##*/
+            /*##############################################*/
 
-            Console.WriteLine("TEST 2 : Guards methods");
-            Console.WriteLine("______________________________\n");
-            TestGuard.TestGuardMethods();
-            Console.WriteLine("Press any key to continue...");
-            Console.ReadKey();
-            Console.Clear();
-
-            Console.WriteLine("TEST 3 : Lions initialization");
-            Console.WriteLine("______________________________\n");
-            TestLion.InitializingLions();
-            Console.WriteLine("Press any key to continue...");
-            Console.ReadKey();
-            Console.Clear();
-
-            Console.WriteLine("TEST 4 : Lions methods");
-            Console.WriteLine("______________________________\n");
-            TestLion.TestLionMethods();
-            Console.WriteLine("Press any key to continue...");
-            Console.ReadKey();
-            Console.Clear();
-
-            Console.WriteLine("TEST 5 : Parrots initialization");
-            Console.WriteLine("______________________________\n");
-            TestParrot.InitializingParrots();
-            Console.WriteLine("Press any key to continue...");
-            Console.ReadKey();
-            Console.Clear();
-
-            Console.WriteLine("TEST 6 : Parrots methods");
-            Console.WriteLine("______________________________\n");
-            TestParrot.TestParrotMethods();
-            Console.WriteLine("Press any key to continue...");
-            Console.ReadKey();
-            Console.Clear();
-
-            Console.WriteLine("TEST 7 : Rabbits initialization");
-            Console.WriteLine("______________________________\n");
-            TestRabbit.InitializingRabbits();
-            Console.WriteLine("Press any key to continue...");
-            Console.ReadKey();
-            Console.Clear();
-
-            Console.WriteLine("TEST 8 : Rabbits methods");
-            Console.WriteLine("______________________________\n");
-            TestRabbit.TestRabbitMethods();
-            Console.WriteLine("Press any key to continue...");
-            Console.ReadKey();
-            Console.Clear();
+            TestAllClasses.All();
         }
     }
 }
