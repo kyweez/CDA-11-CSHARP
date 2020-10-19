29a44dde0315c20b89bb5ced570d588e1629c884
kyweez <auboudier.pro@gmail.com>
2020-10-19 18:32:07 +0200

    LISTBOX / Init FRONT-END

--- EXERCISES/08-LISTBOX/LISTBOX/ListBoxExo.cs
+++ EXERCISES/08-LISTBOX/LISTBOX/ListBoxExo.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace LISTBOX
 {
-    public partial class Form1 : Form
+    public partial class ListBoxExo : Form
     {
-        public Form1()
+        public ListBoxExo()
         {
             InitializeComponent();
         }
