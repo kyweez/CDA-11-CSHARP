ae5b7e2440ff82d0545e4ba71b3f0fbd786eb796
kyweez <auboudier.pro@gmail.com>
2020-10-20 07:01:47 +0200

    INPUT-VALIDATION-V1 / OK

---
+++ EXERCISES/09-INPUT-VALIDATION/INPUT-VALIDATION-V1/INPUT-VALIDATION-V1/InputValidation.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace INPUT_VALIDATION_V1
+{
+    partial class InputValidation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.tbDate = new System.Windows.Forms.TextBox();
+            this.tbAmount = new System.Windows.Forms.TextBox();
+            this.tbZipCode = new System.Windows.Forms.TextBox();
+            this.btnValidate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelDate = new System.Windows.Forms.Label();
+            this.labelDateTip = new System.Windows.Forms.Label();
+            this.labelAmount = new System.Windows.Forms.Label();
+            this.labelZipCode = new System.Windows.Forms.Label();
+            this.errorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(93, 29);
+            this.tbName.MaxLength = 30;
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(261, 20);
+            this.tbName.TabIndex = 0;
+            this.tbName.TextChanged += new System.EventHandler(this.TextBox_TextChanged);
+            // 
+            // tbDate
+            // 
+            this.tbDate.Location = new System.Drawing.Point(93, 80);
+            this.tbDate.Name = "tbDate";
+            this.tbDate.Size = new System.Drawing.Size(154, 20);
+            this.tbDate.TabIndex = 1;
+            this.tbDate.TextChanged += new System.EventHandler(this.TextBox_TextChanged);
+            // 
+            // tbAmount
+            // 
+            this.tbAmount.Location = new System.Drawing.Point(93, 132);
+            this.tbAmount.Name = "tbAmount";
+            this.tbAmount.Size = new System.Drawing.Size(154, 20);
+            this.tbAmount.TabIndex = 2;
+            this.tbAmount.TextChanged += new System.EventHandler(this.TextBox_TextChanged);
+            // 
+            // tbZipCode
+            // 
+            this.tbZipCode.Location = new System.Drawing.Point(93, 183);
+            this.tbZipCode.Name = "tbZipCode";
+            this.tbZipCode.Size = new System.Drawing.Size(71, 20);
+            this.tbZipCode.TabIndex = 3;
+            this.tbZipCode.TextChanged += new System.EventHandler(this.TextBox_TextChanged);
+            // 
+            // btnValidate
+            // 
+            this.btnValidate.Enabled = false;
+            this.btnValidate.Location = new System.Drawing.Point(279, 176);
+            this.btnValidate.Name = "btnValidate";
+            this.btnValidate.Size = new System.Drawing.Size(75, 23);
+            this.btnValidate.TabIndex = 4;
+            this.btnValidate.Text = "OK";
+            this.btnValidate.UseVisualStyleBackColor = true;
+            this.btnValidate.Click += new System.EventHandler(this.BtnValidate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(279, 217);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.BtnDelete_Click);
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(23, 32);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(35, 13);
+            this.labelName.TabIndex = 6;
+            this.labelName.Text = "Name";
+            // 
+            // labelDate
+            // 
+            this.labelDate.AutoSize = true;
+            this.labelDate.Location = new System.Drawing.Point(23, 83);
+            this.labelDate.Name = "labelDate";
+            this.labelDate.Size = new System.Drawing.Size(30, 13);
+            this.labelDate.TabIndex = 7;
+            this.labelDate.Text = "Date";
+            // 
+            // labelDateTip
+            // 
+            this.labelDateTip.AutoSize = true;
+            this.labelDateTip.Location = new System.Drawing.Point(275, 83);
+            this.labelDateTip.Name = "labelDateTip";
+            this.labelDateTip.Size = new System.Drawing.Size(79, 13);
+            this.labelDateTip.TabIndex = 8;
+            this.labelDateTip.Text = "(JJ/MM/AAAA)";
+            // 
+            // labelAmount
+            // 
+            this.labelAmount.AutoSize = true;
+            this.labelAmount.Location = new System.Drawing.Point(23, 135);
+            this.labelAmount.Name = "labelAmount";
+            this.labelAmount.Size = new System.Drawing.Size(43, 13);
+            this.labelAmount.TabIndex = 9;
+            this.labelAmount.Text = "Amount";
+            // 
+            // labelZipCode
+            // 
+            this.labelZipCode.AutoSize = true;
+            this.labelZipCode.Location = new System.Drawing.Point(23, 186);
+            this.labelZipCode.Name = "labelZipCode";
+            this.labelZipCode.Size = new System.Drawing.Size(50, 13);
+            this.labelZipCode.TabIndex = 10;
+            this.labelZipCode.Text = "Zip Code";
+            // 
+            // errorProvider
+            // 
+            this.errorProvider.BlinkStyle = System.Windows.Forms.ErrorBlinkStyle.NeverBlink;
+            this.errorProvider.ContainerControl = this;
+            // 
+            // InputValidation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(393, 263);
+            this.Controls.Add(this.labelZipCode);
+            this.Controls.Add(this.labelAmount);
+            this.Controls.Add(this.labelDateTip);
+            this.Controls.Add(this.labelDate);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnValidate);
+            this.Controls.Add(this.tbZipCode);
+            this.Controls.Add(this.tbAmount);
+            this.Controls.Add(this.tbDate);
+            this.Controls.Add(this.tbName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "InputValidation";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Controls";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.InputValidation_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.TextBox tbDate;
+        private System.Windows.Forms.TextBox tbAmount;
+        private System.Windows.Forms.TextBox tbZipCode;
+        private System.Windows.Forms.Button btnValidate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelDate;
+        private System.Windows.Forms.Label labelDateTip;
+        private System.Windows.Forms.Label labelAmount;
+        private System.Windows.Forms.Label labelZipCode;
+        private System.Windows.Forms.ErrorProvider errorProvider;
+    }
+}
---
+++ EXERCISES/BILL/Bill.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Globalization;
+using TOOLS.ClassLibrary;
+
+namespace BILL
+{
+    public class Bill
+    {
+        private string name;
+        private DateTime date;
+        private double amount;
+        private string zipCode;
+
+        public Bill(string _name, string _date, string _amount, string _zipcode)
+        {
+            // NAME
+            if (_name == null || !CheckData.IsValidName(_name))
+                name = "";
+            else
+                name = _name;
+
+            // DATE
+            if (_date == null || !CheckData.IsValidDate(_date))
+                date = DateTime.Now;
+            else
+                date = DateTime.ParseExact(_date, "dd/MM/yyyy", CultureInfo.CurrentCulture);
+
+            // AMOUNT
+            if (_amount == null || !CheckData.IsValidAmount(_amount))
+                amount = 0;
+            else
+                amount = double.Parse(_amount);
+
+            // ZIPCODE
+            if (_zipcode == null || !CheckData.IsValidZipCode(_zipcode))
+                zipCode = "00000";
+            else
+                zipCode = _zipcode;
+        }
+
+        /// <summary>
+        ///     This methods overrides the original Tostring method
+        ///     Display the current object state
+        /// </summary>
+        public override string ToString()
+        {
+            return
+                $"Name : {name}\n" +
+                $"Date : {date:dd/MM/yyyy}\n" +
+                $"Amount : {Math.Round(amount, 2)}\n" +
+                $"Zip Code : {zipCode}";
+        }
+    }
+}
