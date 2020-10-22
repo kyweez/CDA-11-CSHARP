e7872cc64592bb14fb96489af7658c62e471efdc
kyweez <auboudier.pro@gmail.com>
2020-10-22 14:49:18 +0200

    COMBOBOX / Update

--- EXERCISES/10-LISTS/ComboBoxApp/ComboBoxApp.Designer.cs
+++ EXERCISES/10-LISTS/ComboBoxApp/ComboBoxApp.Designer.cs
@@ -28,27 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ComboBoxApp));
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.cbSource = new System.Windows.Forms.ComboBox();
             this.btnRightShift = new System.Windows.Forms.Button();
             this.btnRightShiftAll = new System.Windows.Forms.Button();
             this.btnLeftShift = new System.Windows.Forms.Button();
             this.btnLeftShiftAll = new System.Windows.Forms.Button();
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.lbTarget = new System.Windows.Forms.ListBox();
             this.btnMoveUp = new System.Windows.Forms.Button();
             this.btnMoveDown = new System.Windows.Forms.Button();
             this.labSource = new System.Windows.Forms.Label();
             this.labTarget = new System.Windows.Forms.Label();
+            this.errorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).BeginInit();
             this.SuspendLayout();
             // 
-            // comboBox1
+            // cbSource
             // 
-            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(23, 48);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(208, 24);
-            this.comboBox1.TabIndex = 0;
+            this.cbSource.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbSource.FormattingEnabled = true;
+            this.cbSource.Location = new System.Drawing.Point(23, 48);
+            this.cbSource.Name = "cbSource";
+            this.cbSource.Size = new System.Drawing.Size(208, 24);
+            this.cbSource.TabIndex = 0;
+            this.cbSource.TextChanged += new System.EventHandler(this.CbSource_TextChanged);
             // 
             // btnRightShift
             // 
@@ -59,6 +63,7 @@
             this.btnRightShift.TabIndex = 1;
             this.btnRightShift.Text = ">";
             this.btnRightShift.UseVisualStyleBackColor = true;
+            this.btnRightShift.Click += new System.EventHandler(this.BtnRightShift_Click);
             // 
             // btnRightShiftAll
             // 
@@ -69,9 +74,11 @@
             this.btnRightShiftAll.TabIndex = 2;
             this.btnRightShiftAll.Text = ">>";
             this.btnRightShiftAll.UseVisualStyleBackColor = true;
+            this.btnRightShiftAll.Click += new System.EventHandler(this.BtnRightShiftAll_Click);
             // 
             // btnLeftShift
             // 
+            this.btnLeftShift.Enabled = false;
             this.btnLeftShift.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnLeftShift.Location = new System.Drawing.Point(308, 156);
             this.btnLeftShift.Name = "btnLeftShift";
@@ -79,9 +86,11 @@
             this.btnLeftShift.TabIndex = 3;
             this.btnLeftShift.Text = "<";
             this.btnLeftShift.UseVisualStyleBackColor = true;
+            this.btnLeftShift.Click += new System.EventHandler(this.BtnLeftShift_Click);
             // 
             // btnLeftShiftAll
             // 
+            this.btnLeftShiftAll.Enabled = false;
             this.btnLeftShiftAll.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnLeftShiftAll.Location = new System.Drawing.Point(308, 198);
             this.btnLeftShiftAll.Name = "btnLeftShiftAll";
@@ -89,19 +98,22 @@
             this.btnLeftShiftAll.TabIndex = 4;
             this.btnLeftShiftAll.Text = "<<";
             this.btnLeftShiftAll.UseVisualStyleBackColor = true;
+            this.btnLeftShiftAll.Click += new System.EventHandler(this.BtnLeftShiftAll_Click);
             // 
-            // listBox1
+            // lbTarget
             // 
-            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(445, 48);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(203, 180);
-            this.listBox1.TabIndex = 5;
+            this.lbTarget.Enabled = false;
+            this.lbTarget.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTarget.FormattingEnabled = true;
+            this.lbTarget.ItemHeight = 16;
+            this.lbTarget.Location = new System.Drawing.Point(445, 48);
+            this.lbTarget.Name = "lbTarget";
+            this.lbTarget.Size = new System.Drawing.Size(203, 180);
+            this.lbTarget.TabIndex = 5;
             // 
             // btnMoveUp
             // 
+            this.btnMoveUp.Enabled = false;
             this.btnMoveUp.Image = ((System.Drawing.Image)(resources.GetObject("btnMoveUp.Image")));
             this.btnMoveUp.Location = new System.Drawing.Point(499, 250);
             this.btnMoveUp.Name = "btnMoveUp";
@@ -111,6 +123,7 @@
             // 
             // btnMoveDown
             // 
+            this.btnMoveDown.Enabled = false;
             this.btnMoveDown.Image = ((System.Drawing.Image)(resources.GetObject("btnMoveDown.Image")));
             this.btnMoveDown.Location = new System.Drawing.Point(556, 250);
             this.btnMoveDown.Name = "btnMoveDown";
@@ -131,12 +144,17 @@
             // labTarget
             // 
             this.labTarget.AutoSize = true;
+            this.labTarget.Enabled = false;
             this.labTarget.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labTarget.Location = new System.Drawing.Point(441, 14);
             this.labTarget.Name = "labTarget";
             this.labTarget.Size = new System.Drawing.Size(61, 20);
             this.labTarget.TabIndex = 9;
             this.labTarget.Text = "Target";
+            // 
+            // errorProvider
+            // 
+            this.errorProvider.ContainerControl = this;
             // 
             // ComboBoxApp
             // 
@@ -147,14 +165,15 @@
             this.Controls.Add(this.labSource);
             this.Controls.Add(this.btnMoveDown);
             this.Controls.Add(this.btnMoveUp);
-            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.lbTarget);
             this.Controls.Add(this.btnLeftShiftAll);
             this.Controls.Add(this.btnLeftShift);
             this.Controls.Add(this.btnRightShiftAll);
             this.Controls.Add(this.btnRightShift);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.cbSource);
             this.Name = "ComboBoxApp";
             this.Text = "ComboBox & ListBox";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -162,16 +181,17 @@
 
         #endregion
 
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox cbSource;
         private System.Windows.Forms.Button btnRightShift;
         private System.Windows.Forms.Button btnRightShiftAll;
         private System.Windows.Forms.Button btnLeftShift;
         private System.Windows.Forms.Button btnLeftShiftAll;
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox lbTarget;
         private System.Windows.Forms.Button btnMoveUp;
         private System.Windows.Forms.Button btnMoveDown;
         private System.Windows.Forms.Label labSource;
         private System.Windows.Forms.Label labTarget;
+        private System.Windows.Forms.ErrorProvider errorProvider;
     }
 }
 
--- EXERCISES/10-LISTS/ComboBoxApp/ComboBoxApp.cs
+++ EXERCISES/10-LISTS/ComboBoxApp/ComboBoxApp.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,10 +13,137 @@ namespace ComboBoxApp
 {
     public partial class ComboBoxApp : Form
     {
+        /***************************** ATTRIBUTES ****************************/
+        private List<string> countryTab;
+
+        /**************************** CONSTRUCTORS ***************************/
         public ComboBoxApp()
         {
             InitializeComponent();
+            countryTab = new List<string> { "France", "Belgique", "Allemagne", "Japon", "Portugal", "Grece", "Bulgarie", "Espagne" };
+            countryTab.Sort();
+            CopyAllToSource();
         }
+
+        /***************************** PROPERTIES ****************************/
+
+        /****************************** METHODS ******************************/
+
+        /// <summary>
+        /// This method adds a country to the targetBox and update the countryTab
+        /// </summary>
+        /// <param name="_country">string</param>
+        private void AddCountry(string _country)
+        {
+            if (_country.Length > 30)
+                errorProvider.SetError(cbSource, "Max lenght country name == 30");
+            else if (!Regex.IsMatch(_country, @"^[a-zA-Z]+([\-\' ][a-zA-Z]+)*$"))
+                errorProvider.SetError(cbSource, "Doesn't respect the classical country name convention");
+            else
+            {
+                lbTarget.Items.Add(_country);
+                countryTab.Add(_country);
+            }
+        }
+
+        /// <summary>
+        /// This method enable or disable buttons depending on box are empty or not
+        /// </summary>
+        private void CheckIsEmptyBox()
+        {
+            if (cbSource.Items.Count == 0)
+                btnRightShiftAll.Enabled = false;
+            else
+                btnRightShiftAll.Enabled = true;
+            if (lbTarget.Items.Count == 0)
+            {
+                btnLeftShift.Enabled = false;
+                btnLeftShiftAll.Enabled = false;
+                lbTarget.Enabled = false;
+                labTarget.Enabled = false;
+            }
+            else
+            {
+                btnLeftShift.Enabled = true;
+                btnLeftShiftAll.Enabled = true;
+                lbTarget.Enabled = true;
+                labTarget.Enabled = true;
+            }
+        }
+
+        /// <summary>
+        /// This method moves all countries to source comboBox
+        /// </summary>
+        private void CopyAllToSource()
+        {
+            foreach (string country in countryTab)
+                cbSource.Items.Add(country);
+            lbTarget.Items.Clear();
+        }
+
+        /// <summary>
+        /// This method moves all countries to target listBox
+        /// </summary>
+        private void CopyAllToTarget()
+        {
+            foreach (string country in countryTab)
+                lbTarget.Items.Add(country);
+            cbSource.Items.Clear();
+        }
+
+        private void SwapCountryFromSourceToTarget(string _country)
+        {
+            if (cbSource.Items.Contains(_country))
+            {
+                lbTarget.Items.Add(_country);
+                cbSource.Items.Remove(_country);
+            }
+            else
+            {
+                if (lbTarget.Items.Contains(_country))
+                    errorProvider.SetError(cbSource, "This country already exists");
+                else
+                    AddCountry(_country);
+            }
+        }
+
+        /******************************* EVENTS ******************************/
+
+        private void BtnLeftShift_Click(object sender, EventArgs e)
+        {
+            CheckIsEmptyBox();
+        }
+
+        private void BtnLeftShiftAll_Click(object sender, EventArgs e)
+        {
+            CopyAllToSource();
+            CheckIsEmptyBox();
+        }
+
+        private void BtnRightShift_Click(object sender, EventArgs e)
+        {
+            CheckIsEmptyBox();
+        }
+
+        private void BtnRightShiftAll_Click(object sender, EventArgs e)
+        {
+            CopyAllToTarget();
+            CheckIsEmptyBox();
+        }
+
+        private void CbSource_TextChanged(object sender, EventArgs e)
+        {
+            errorProvider.Clear();
+            if (cbSource.Text.Length == 0)
+                btnRightShift.Enabled = false;
+        }
+
+
+
+
+
+
+
 
     }
 }
