71774997da4e01cfa14d2b56a88364e637767a19
kyweez <auboudier.pro@gmail.com>
2020-10-27 11:05:29 +0100

    SYNTHESIS / Person class update

--- EXERCISES/12-SYNTHESIS/PersonClassLibrary/Person.cs
+++ EXERCISES/12-SYNTHESIS/PersonClassLibrary/Person.cs
@@ -17,13 +17,6 @@ namespace PersonClassLibrary
         private SexType sex;
         #endregion
 
-
-        //   T:System.ArgumentNullException:
-        //     input or pattern is null.
-        //
-        //   T:System.Text.RegularExpressions.RegexMatchTimeoutException:
-        //     A time-out occurred. For more information about time-outs, see the Remarks section.
-
         #region Constructor
         /// <summary>
         /// Create a new person object
@@ -32,24 +25,23 @@ namespace PersonClassLibrary
         /// <param name="_firstName">string</param>
         /// <param name="_dateOfBirth">string</param>
         /// <param name="_sex">SexType</param>
+        /// <exception cref="ArgumentNullException">Input or pattern is null</exception>
+        /// <exception cref="RegexMatchTimeoutException">A time-out occurred</exception>
         /// <exception cref="BadNameInputFormatException">Input names don't fit to the given regex</exception>
         /// <exception cref="BadDateStringFormatException">The input string doesn't have a good format</exception>
         /// <exception cref="InvalidDateOfBirthException">The input date of birth is not valid</exception>
-        /// <exception cref="ArgumentNullException">Input or pattern is null</exception>
-        /// <exception cref="RegexMatchTimeoutException">A time-out occurred</exception>
-        /// 
         public Person(string _lastName, string _firstName, string _dateOfBirth, SexType _sex)
         {
             try
             {
                 if (!Regex.IsMatch(_lastName, REGEX_NAME) || Regex.IsMatch(_firstName, REGEX_NAME))
                     throw new BadNameInputFormatException();
-                lastName = _lastName;
-                firstName = _firstName;
                 if (!DateTime.TryParse(_dateOfBirth, out DateTime result))
                     throw new BadDateStringFormatException();
                 else if (result > DateTime.Now || result.Year < 1900)
                     throw new InvalidDateOfBirthException();
+                lastName = _lastName;
+                firstName = _firstName;
                 dateOfBirth = result;
                 sex = _sex;
             }
