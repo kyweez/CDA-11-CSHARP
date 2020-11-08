5329a7077d72525a1322f3c78367415a3fafaac8
kyweez <auboudier.pro@gmail.com>
2020-11-08 21:17:39 +0100

    LOAN-v2 / Init

---
+++ EXERCISES/LoanClassLibrary/LoanExceptions/BadNameInputFormatException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LoanClassLibrary.LoanExceptions
+{
+    public class BadNameInputFormatException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the System.Exception class with a specified error message.
+        /// Message : "Invalid name format"
+        /// </summary>
+        public BadNameInputFormatException() : base("Invalid name format")
+        {
+            HelpLink = "https://docs.microsoft.com";
+            Source = "PersonClassLibrary.Person.cs";
+        }
+    }
+}
---
+++ EXERCISES/LoanClassLibrary/LoanExceptions/InvalidRepaymentPeriodicityException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoanClassLibrary.LoanExceptions
+{
+    public class InvalidRepaymentPeriodicityException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the System.Exception class with a specified error message.
+        /// Message : "Invalid repayment periodicity"
+        /// </summary>
+        public InvalidRepaymentPeriodicityException() : base("Invalid repayment periodicity")
+        {
+            HelpLink = "https://docs.microsoft.com";
+            Source = "LoanClassLibrary.LoanExceptions.cs";
+        }
+    }
+}
