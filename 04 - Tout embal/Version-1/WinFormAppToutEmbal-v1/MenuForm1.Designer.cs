a0b87a608db8d81a61e83815ee7be6c14fdd3b90
kyweez <auboudier.pro@gmail.com>
2020-11-15 20:39:29 +0100

    Tout embal v1 / Menu strip init

--- 04 - Tout embal/Version-1/WinFormAppToutEmbal-v1/MenuForm1.Designer.cs
+++ 04 - Tout embal/Version-1/WinFormAppToutEmbal-v1/MenuForm1.Designer.cs
@@ -28,24 +28,159 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MenuForm1));
+            this.menuStripInMenuForm = new System.Windows.Forms.MenuStrip();
+            this.connectionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.consoleApplicationsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.firstStepsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.inputValidationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.graphicalObjectsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.windowsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.logInToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.butterflyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zooToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.forestToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStripInMenuForm.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStripInMenuForm
+            // 
+            this.menuStripInMenuForm.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.connectionToolStripMenuItem,
+            this.consoleApplicationsToolStripMenuItem,
+            this.firstStepsToolStripMenuItem,
+            this.inputValidationToolStripMenuItem,
+            this.graphicalObjectsToolStripMenuItem,
+            this.windowsToolStripMenuItem});
+            this.menuStripInMenuForm.Location = new System.Drawing.Point(0, 0);
+            this.menuStripInMenuForm.Name = "menuStripInMenuForm";
+            this.menuStripInMenuForm.Size = new System.Drawing.Size(800, 24);
+            this.menuStripInMenuForm.TabIndex = 1;
+            this.menuStripInMenuForm.Text = "menuStrip1";
+            // 
+            // connectionToolStripMenuItem
+            // 
+            this.connectionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.logInToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.quitToolStripMenuItem});
+            this.connectionToolStripMenuItem.Name = "connectionToolStripMenuItem";
+            this.connectionToolStripMenuItem.Size = new System.Drawing.Size(81, 20);
+            this.connectionToolStripMenuItem.Text = "Connection";
+            // 
+            // consoleApplicationsToolStripMenuItem
+            // 
+            this.consoleApplicationsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.butterflyToolStripMenuItem,
+            this.zooToolStripMenuItem,
+            this.forestToolStripMenuItem});
+            this.consoleApplicationsToolStripMenuItem.Enabled = false;
+            this.consoleApplicationsToolStripMenuItem.Name = "consoleApplicationsToolStripMenuItem";
+            this.consoleApplicationsToolStripMenuItem.Size = new System.Drawing.Size(129, 20);
+            this.consoleApplicationsToolStripMenuItem.Text = "Console applications";
+            // 
+            // firstStepsToolStripMenuItem
+            // 
+            this.firstStepsToolStripMenuItem.Enabled = false;
+            this.firstStepsToolStripMenuItem.Name = "firstStepsToolStripMenuItem";
+            this.firstStepsToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.firstStepsToolStripMenuItem.Text = "First steps";
+            // 
+            // inputValidationToolStripMenuItem
+            // 
+            this.inputValidationToolStripMenuItem.Enabled = false;
+            this.inputValidationToolStripMenuItem.Name = "inputValidationToolStripMenuItem";
+            this.inputValidationToolStripMenuItem.Size = new System.Drawing.Size(102, 20);
+            this.inputValidationToolStripMenuItem.Text = "Input validation";
+            // 
+            // graphicalObjectsToolStripMenuItem
+            // 
+            this.graphicalObjectsToolStripMenuItem.Enabled = false;
+            this.graphicalObjectsToolStripMenuItem.Name = "graphicalObjectsToolStripMenuItem";
+            this.graphicalObjectsToolStripMenuItem.Size = new System.Drawing.Size(110, 20);
+            this.graphicalObjectsToolStripMenuItem.Text = "Graphical objects";
+            // 
+            // windowsToolStripMenuItem
+            // 
+            this.windowsToolStripMenuItem.Enabled = false;
+            this.windowsToolStripMenuItem.Name = "windowsToolStripMenuItem";
+            this.windowsToolStripMenuItem.Size = new System.Drawing.Size(68, 20);
+            this.windowsToolStripMenuItem.Text = "Windows";
+            // 
+            // logInToolStripMenuItem
+            // 
+            this.logInToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("logInToolStripMenuItem.Image")));
+            this.logInToolStripMenuItem.Name = "logInToolStripMenuItem";
+            this.logInToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.logInToolStripMenuItem.Text = "Log in";
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("quitToolStripMenuItem.Image")));
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.quitToolStripMenuItem.Text = "Quit";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // butterflyToolStripMenuItem
+            // 
+            this.butterflyToolStripMenuItem.Name = "butterflyToolStripMenuItem";
+            this.butterflyToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.butterflyToolStripMenuItem.Text = "Butterfly";
+            // 
+            // zooToolStripMenuItem
+            // 
+            this.zooToolStripMenuItem.Name = "zooToolStripMenuItem";
+            this.zooToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.zooToolStripMenuItem.Text = "Zoo";
+            // 
+            // forestToolStripMenuItem
+            // 
+            this.forestToolStripMenuItem.Name = "forestToolStripMenuItem";
+            this.forestToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.forestToolStripMenuItem.Text = "Forest";
             // 
             // MenuForm1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStripInMenuForm);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStripInMenuForm;
             this.MaximizeBox = false;
             this.Name = "MenuForm1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Create forms";
+            this.menuStripInMenuForm.ResumeLayout(false);
+            this.menuStripInMenuForm.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStripInMenuForm;
+        private System.Windows.Forms.ToolStripMenuItem connectionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem consoleApplicationsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem firstStepsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem inputValidationToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem graphicalObjectsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem windowsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem logInToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem butterflyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zooToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem forestToolStripMenuItem;
     }
 }
 
