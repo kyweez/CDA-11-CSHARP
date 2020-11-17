65988ccb29d300c100f7f8a7bacae8823d58ab6a
kyweez <auboudier.pro@gmail.com>
2020-11-17 11:42:19 +0100

    Event / Init

--- 05 - Courses exercises/01 - Events/ClassLibraryPerson/Person.cs
+++ 05 - Courses exercises/01 - Events/ClassLibraryPerson/Person.cs
@@ -4,5 +4,33 @@ namespace ClassLibraryPerson
 {
     public class Person
     {
+        private string Name
+        {
+            get;
+            set;
+        }
+
+        public byte Age
+        {
+            get;
+            set;
+        }
+
+        public delegate void DelegateMajorLegalAge(Person sender);
+
+        public Person(string _name, byte _age)
+        {
+            Name = _name;
+            Age = _age;
+        }
+
+        public Person(string _name) : this(_name, 0)
+        {            
+        }
+
+        public Person() : this("John Doe")
+        {
+        
+        }
     }
 }
