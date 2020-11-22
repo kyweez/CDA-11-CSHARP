32a571f90ad0f6c2189f6ea266ead67e9050f36a
kyweez <auboudier.pro@gmail.com>
2020-11-23 00:37:53 +0100

    MDI / Login OK + Commented

---
+++ 04 - MDI/WinFormAppMDI/LoginForm.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace WinFormAppMDI
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBoxLogin = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.buttonValidate = new System.Windows.Forms.Button();
+            this.labelLogin = new System.Windows.Forms.Label();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.errorProviderLogin = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProviderPassword = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderLogin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderPassword)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxLogin
+            // 
+            this.textBoxLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.textBoxLogin.Location = new System.Drawing.Point(124, 19);
+            this.textBoxLogin.Margin = new System.Windows.Forms.Padding(10);
+            this.textBoxLogin.MaxLength = 30;
+            this.textBoxLogin.Name = "textBoxLogin";
+            this.textBoxLogin.Size = new System.Drawing.Size(211, 24);
+            this.textBoxLogin.TabIndex = 0;
+            this.textBoxLogin.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            this.textBoxLogin.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_KeyPress);
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.textBoxPassword.Location = new System.Drawing.Point(124, 63);
+            this.textBoxPassword.Margin = new System.Windows.Forms.Padding(10);
+            this.textBoxPassword.MaxLength = 30;
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(211, 24);
+            this.textBoxPassword.TabIndex = 1;
+            this.textBoxPassword.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            this.textBoxPassword.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_KeyPress);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.buttonCancel.Location = new System.Drawing.Point(124, 107);
+            this.buttonCancel.Margin = new System.Windows.Forms.Padding(10);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(83, 32);
+            this.buttonCancel.TabIndex = 2;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            // 
+            // buttonValidate
+            // 
+            this.buttonValidate.Enabled = false;
+            this.buttonValidate.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.buttonValidate.Location = new System.Drawing.Point(252, 107);
+            this.buttonValidate.Margin = new System.Windows.Forms.Padding(10);
+            this.buttonValidate.Name = "buttonValidate";
+            this.buttonValidate.Size = new System.Drawing.Size(83, 32);
+            this.buttonValidate.TabIndex = 3;
+            this.buttonValidate.Text = "OK";
+            this.buttonValidate.UseVisualStyleBackColor = true;
+            this.buttonValidate.Click += new System.EventHandler(this.buttonValidate_Click);
+            // 
+            // labelLogin
+            // 
+            this.labelLogin.AutoSize = true;
+            this.labelLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.labelLogin.Location = new System.Drawing.Point(22, 22);
+            this.labelLogin.Margin = new System.Windows.Forms.Padding(10);
+            this.labelLogin.Name = "labelLogin";
+            this.labelLogin.Size = new System.Drawing.Size(44, 18);
+            this.labelLogin.TabIndex = 4;
+            this.labelLogin.Text = "Login";
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.labelPassword.Location = new System.Drawing.Point(22, 66);
+            this.labelPassword.Margin = new System.Windows.Forms.Padding(10);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(75, 18);
+            this.labelPassword.TabIndex = 5;
+            this.labelPassword.Text = "Password";
+            // 
+            // errorProviderLogin
+            // 
+            this.errorProviderLogin.BlinkStyle = System.Windows.Forms.ErrorBlinkStyle.NeverBlink;
+            this.errorProviderLogin.ContainerControl = this;
+            // 
+            // errorProviderPassword
+            // 
+            this.errorProviderPassword.BlinkStyle = System.Windows.Forms.ErrorBlinkStyle.NeverBlink;
+            this.errorProviderPassword.ContainerControl = this;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(366, 153);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.labelLogin);
+            this.Controls.Add(this.buttonValidate);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxLogin);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "LoginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Connection";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderLogin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderPassword)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxLogin;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Button buttonValidate;
+        private System.Windows.Forms.Label labelLogin;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.ErrorProvider errorProviderLogin;
+        private System.Windows.Forms.ErrorProvider errorProviderPassword;
+    }
+}
---
+++ 04 - MDI/WinFormAppMDI/LoginForm.cs
@@ -0,0 +1,120 @@
+﻿using ClassLibraryLogin;
+using System;
+using System.Windows.Forms;
+
+namespace WinFormAppMDI
+{
+    public partial class LoginForm : Form
+    {
+        #region ############### PROPERTY ###############
+        private Login CurrentLogin
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region ############### CONSTRUCTOR ###############
+        public LoginForm(Login _login)
+        {
+            InitializeComponent();
+            CurrentLogin = _login;
+        }
+        #endregion
+
+        #region ############### METHODS ###############
+        /// <summary>
+        /// This method makes the current form unmovable
+        /// </summary>
+        /// <param name="m">Message</param>
+        protected override void WndProc(ref Message m)
+        {
+            const int WM_SYSCOMMAND = 0x0112;
+            const int SC_MOVE = 0xF010;
+
+            switch (m.Msg)
+            {
+                case WM_SYSCOMMAND:
+                    int command = m.WParam.ToInt32() & 0xfff0;
+                    if (command == SC_MOVE)
+                        return;
+                    break;
+            }
+            base.WndProc(ref m);
+        }
+
+        /// <summary>
+        /// This method tries to connect with the login and password textboxes information given by user
+        /// If the authentication failed, inform and reset the form.
+        /// Else, inform user and close the form
+        /// </summary>
+        private void TryToConnect()
+        {
+            CurrentLogin.ConnectionAttempt(textBoxLogin.Text, textBoxPassword.Text);
+            if (!CurrentLogin.IsValidLogin)
+            {
+                MessageBox.Show("Authentication failure, try again !", "Authentication", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBoxLogin.ResetText();
+                textBoxPassword.ResetText();
+                ActiveControl = textBoxLogin;
+            }
+            else
+            {
+                MessageBox.Show("Authentication success, welcome on board !", "Authentication", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Close();
+            }
+        }
+        #endregion
+
+        #region ############### EVENTS ###############
+        /// <summary>
+        /// This event is triggered when a textbox content changes.
+        /// Reset the linked erroprovider and enable/disable the OK button
+        /// </summary>
+        /// <param name="sender">object</param>
+        /// <param name="e">EventArgs</param>
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+            if ((TextBox)sender == textBoxLogin)
+                errorProviderLogin.Clear();
+            if ((TextBox)sender == textBoxPassword)
+                errorProviderPassword.Clear();
+            if (textBoxLogin.Text.Length == 0 || textBoxPassword.Text.Length == 0)
+                buttonValidate.Enabled = false;
+            else
+                buttonValidate.Enabled = true;
+        }
+
+        /// <summary>
+        /// This event is triggered when the OK button is clicked.
+        /// Try to connect with the login and password textboxes information given by user
+        /// </summary>
+        /// <param name="sender">object</param>
+        /// <param name="e">EventArgs</param>
+        private void buttonValidate_Click(object sender, EventArgs e)
+        {
+            TryToConnect();
+        }
+
+        /// <summary>
+        /// This event is triggered when a key is pressed.
+        /// If the key is return, it tries to connect with the login and password textboxes information given by user
+        /// If one of them is empty, it doesn't trigger the connection but it displays erroProviders
+        /// </summary>
+        /// <param name="sender">object</param>
+        /// <param name="e">KeyPressEventArgs</param
+        private void textBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Return)
+            {
+                if (textBoxLogin.Text.Length == 0)
+                    errorProviderLogin.SetError(textBoxLogin, "Can't be empty !");
+                if (textBoxPassword.Text.Length == 0)
+                    errorProviderPassword.SetError(textBoxPassword, "Can't be empty !");
+                if (textBoxLogin.Text.Length != 0 && textBoxPassword.Text.Length != 0)
+                    TryToConnect();
+            }
+        }
+    }
+    #endregion
+}
