b3c796d9cefd27110a3b9889bc5ed3b9324b762c
kyweez <auboudier.pro@gmail.com>
2020-12-02 10:13:10 +0100

    ToutEmbal / Update HMI

--- 06 - ToutEmbal/Version 1/WinFormAppToutEmbal-v1/Program.cs
+++ 06 - ToutEmbal/Version 1/WinFormAppToutEmbal-v1/Program.cs
@@ -16,7 +16,7 @@ namespace WinFormAppToutEmbal_v1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ToutEmbalForm());
+            Application.Run(new ToutEmbalForm1());
         }
     }
 }
