8e76fdd786930d2c173f70087a8d102de9d8e458
kyweez <auboudier.pro@gmail.com>
2020-12-03 00:36:01 +0100

    ToutEmbal / Crate OK

---
+++ 06 - ToutEmbal/Version 1/ClassLibraryCrateProduction-v1/Crate.cs
@@ -0,0 +1,47 @@
+﻿using ClassLibraryCrateProduction_v1.Exceptions;
+using System;
+
+namespace ClassLibraryCrateProduction_v1
+{
+    public class Crate
+    {
+        #region ############### PROPERTIES & CONSTRUCTOR ###############
+        public Type CrateType
+        {
+            get;
+            private set;
+        }
+
+        public bool IsFaulty
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Crate constructor
+        /// </summary>
+        /// <param name="_type">Type</param>
+        /// <param name="_typeList">TypeList</param>
+        /// <exception cref="ArgumentNullException">Thrown when a given argument is null</exception>
+        /// <exception cref="ImpossibleCrateInstanciation">Thrown when the given type isn't contained in the given typeList</exception>
+        /// <exception cref="Exception">Thrown if the Contains method catched an exception</exception>
+        public Crate(Type _type, TypeList _typeList)
+        {
+            if (_type == null || _typeList == null)
+                throw new ArgumentNullException("The type argument given in the Crate constructor is null");
+            try
+            {
+                if (!_typeList.Contains(_type))
+                    throw new ImpossibleCrateInstanciation("The type given as an argument doesn't exist in the typelist argument");
+                CrateType = _type;
+                IsFaulty = false;
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Impossible crate instanciation", e);
+            }
+        }
+        #endregion
+    }
+}
