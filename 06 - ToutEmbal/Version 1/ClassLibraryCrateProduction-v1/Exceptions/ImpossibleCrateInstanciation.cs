8977a0e174053264b86f65477393f98a7ce47675
kyweez <auboudier.pro@gmail.com>
2020-12-03 00:19:39 +0100

    ToutEmbal / Excepetion creation

---
+++ 06 - ToutEmbal/Version 1/ClassLibraryCrateProduction-v1/Exceptions/ImpossibleCrateInstanciation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ClassLibraryCrateProduction_v1.Exceptions
+{
+    public class ImpossibleCrateInstanciation : Exception
+    {
+        /// <summary>
+        /// Instanciate a new exception when a Crate object can't be instanciated
+        /// </summary>
+        public ImpossibleCrateInstanciation() : base()
+        {
+            HelpLink = "https://docs.microsoft.com";
+            Source = "ClassLibraryCrateProduction_v1";
+        }
+
+        /// <summary>
+        /// Instanciate a new exception when a Crate object can't be instanciated
+        /// </summary>
+        /// <param name="message">string</param>
+        public ImpossibleCrateInstanciation(string message) : base(message)
+        {
+            HelpLink = "https://docs.microsoft.com";
+            Source = "ClassLibraryCrateProduction_v1";
+        }
+
+        /// <summary>
+        /// Instanciate a new exception when a Crate object can't be instanciated
+        /// </summary>
+        /// <param name="message">string</param>
+        /// <param name="inner">Exception</param>
+        public ImpossibleCrateInstanciation(string message, System.Exception inner) : base(message, inner)
+        {
+            HelpLink = "https://docs.microsoft.com";
+            Source = "ClassLibraryCrateProduction_v1";
+        }
+    }
+}
