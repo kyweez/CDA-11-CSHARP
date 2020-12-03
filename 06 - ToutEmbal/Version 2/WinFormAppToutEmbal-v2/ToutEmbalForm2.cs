ab51d7bc5c94962755e437c4265b67f81a3ee932
kyweez <auboudier.pro@gmail.com>
2020-12-03 10:59:46 +0100

    ToutEmbal v2 / Init

---
+++ 06 - ToutEmbal/Version 2/WinFormAppToutEmbal-v2/ToutEmbalForm2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormAppToutEmbal_v2
+{
+    public partial class ToutEmbalForm2 : Form
+    {
+        public ToutEmbalForm2()
+        {
+            InitializeComponent();
+        }
+    }
+}
