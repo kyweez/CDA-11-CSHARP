eeaf15306c96573ca726c13241f8b3fb7950c9b9
kyweez <auboudier.pro@gmail.com>
2020-12-03 01:25:32 +0100

    ToutEmbal / Push before sleep

---
+++ 06 - ToutEmbal/Version 1/ClassLibraryCrateProduction-v1/CrateProduction.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryCrateProduction_v1
+{
+    public class CrateProduction
+    {
+        private List<Crate> Crates
+        {
+            get;
+            set;
+        }
+
+        public string ProductionName
+        {
+            get;
+            private set;
+        }
+
+        public int NumberOfCratesToProduce
+        {
+            get;
+            private set;
+        }
+
+        public int HourlyOutput
+        {
+            get;
+            private set;
+        }
+
+        public ProductionStatusEnum ProductionStatus
+        {
+            get;
+            private set;
+        }
+
+        public int GetCurrentNumberOfCrates()
+        {
+            throw new NotImplementedException(); 
+        }
+
+        public int GetNumberOfFaultyCrates()
+        {
+            throw new NotImplementedException(); 
+        }
+        
+        public int GetNumberOfValidCrates()
+        {
+            throw new NotImplementedException(); 
+        }
+
+        public int GetNumberOfProducedCrates()
+        {
+            throw new NotImplementedException();
+        }
+
+        public float GetFaultyRate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public float GetHourlyFaultyRate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
