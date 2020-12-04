a86b55cbe1f0b38b88cb40d178afc0060249c170
kyweez <auboudier.pro@gmail.com>
2020-12-04 23:50:05 +0100

    Exo Thread / OK

---
+++ 05 - Courses exercises/04 - Thread/ClassLibraryCooking/Cake.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+
+namespace ClassLibraryCooking
+{
+    public class Cake
+    {
+        const int volumeMaxInPercent = 120;
+        int volumeInPerCent;
+        Thread threadInflate;
+        Thread threadCook;
+
+        public Cake()
+        {
+            volumeInPerCent = 10;
+            threadInflate = new Thread(new ThreadStart(Inflate));
+            threadCook = new Thread(new ThreadStart(Cook));
+        }
+
+
+
+        public delegate void DelegateInflateDone(int size);
+        public event DelegateInflateDone InformUserAboutInflation;        
+        public delegate void DelegateCookDone(int size);
+        public event DelegateCookDone InformUserAboutCook;
+
+        private void Inflate()
+        {
+            while (volumeInPerCent != volumeMaxInPercent)
+            {
+                Thread.Sleep(500);
+                volumeInPerCent += 10;
+                Console.WriteLine($"Cake increase : +10 % / Cake volume : {volumeInPerCent}%");
+                if (volumeInPerCent == volumeMaxInPercent && InformUserAboutInflation != null)
+                    InformUserAboutInflation(volumeInPerCent);
+            }
+        }
+
+        public void Finish()
+        {
+            threadInflate.Start();
+            threadCook.Start();
+        }
+
+        private void Cook()
+        {
+            while (volumeInPerCent != volumeMaxInPercent)
+            {
+                Thread.Sleep(700);
+                volumeInPerCent += 10;
+                Console.WriteLine($"Cooking : +10 % / Cake volume : {volumeInPerCent}%");
+                if (volumeInPerCent == volumeMaxInPercent && InformUserAboutInflation != null)
+                    InformUserAboutCook(volumeInPerCent);
+            }
+        }
+    }
+}
---
+++ 05 - Courses exercises/04 - Thread/ConsoleAppThread/Program.cs
@@ -0,0 +1,21 @@
+﻿using ClassLibraryCooking;
+using System;
+
+namespace ConsoleAppThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cake bananaBread = new Cake();
+            bananaBread.InformUserAboutInflation += new Cake.DelegateInflateDone(BananaBread_InformUserAboutInflation);
+            bananaBread.InformUserAboutCook += new Cake.DelegateCookDone(BananaBread_InformUserAboutInflation);
+            bananaBread.Finish();
+        }
+
+        private static void BananaBread_InformUserAboutInflation(int size)
+        {
+            Console.WriteLine($"The cake is inflated at {size}%");        
+        }
+    }
+}
