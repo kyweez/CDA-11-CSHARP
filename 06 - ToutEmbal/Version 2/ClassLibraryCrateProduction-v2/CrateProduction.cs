704748b49f3c80e6627b62a24696c5e04f0a713f
kyweez <auboudier.pro@gmail.com>
2020-12-05 23:52:16 +0100

    ToutEmbal v2 / Event

---
+++ 06 - ToutEmbal/Version 2/ClassLibraryCrateProduction-v2/CrateProduction.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ClassLibraryCrateProduction_v2
+{
+    class CrateProduction
+    {
+        #region ############### PROPERTIES ###############
+        public ProductionStatusEnum ProductionStatus
+        {
+            get;
+            private set;
+        }
+
+        public string ProductionName
+        {
+            get;
+            private set;
+        }
+
+        public DateTime ProductionStartDateTime
+        {
+            get;
+            private set;
+        }
+
+        public int NumberOfValidCratesToProduce
+        {
+            get;
+            private set;
+        }
+
+        public int CurrentNumberOfFaultyCrates
+        {
+            get;
+            private set;
+        }
+
+        public int CurrentNumberOfValidCrates
+        {
+            get;
+            private set;
+        }
+
+        public int HourlyOutputOfCrates
+        {
+            get;
+            private set;
+        }
+
+        public Thread ProduceThread
+        {
+            get;
+            private set;
+        }
+        #endregion
+
+        #region ############### CONSTRUCTOR ###############
+        public CrateProduction(string _productionName, int _numberOfValidCratesToProduce, int _hourlyOutputOfCrates)
+        {
+            ProductionStatus = 0;
+            ProductionName = _productionName;
+            NumberOfValidCratesToProduce = _numberOfValidCratesToProduce;
+            HourlyOutputOfCrates = _hourlyOutputOfCrates;
+            //Thread t = new Thread();
+        }
+        #endregion
+
+        #region ############### EVENTS ###############
+        #endregion
+
+        #region ############### STATUS CHANGE METHODS ###############
+        public bool StartProduction()
+        {
+            if (ProductionStatus != 0)
+                return false;
+            ProduceThread = new Thread(new ThreadStart(RunProduction));
+            ProduceThread.Start();
+            return true;
+            
+        }
+
+        private void RunProduction()
+        {
+            int timeBetweenCrateCreation = (3600 * 60 * 60) / HourlyOutputOfCrates;
+
+            while (ProductionStatus == ProductionStatusEnum.started || ProductionStatus == ProductionStatusEnum.restarted)
+            {
+                Thread.Sleep(timeBetweenCrateCreation);
+                CreateCrate();
+            }
+        }
+
+        private void CreateCrate()
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(0, 100);
+        }
+
+
+        #endregion
+
+    }
+}
---
+++ 06 - ToutEmbal/Version 2/ClassLibraryCrateProduction-v2/ProductionStatusEnum.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace ClassLibraryCrateProduction_v2
+{
+    public enum ProductionStatusEnum
+    {
+        [Description("Not started")]
+        notStarted = 0,
+
+        [Description("Started")]
+        started = 1,
+
+        [Description("Paused")]
+        paused = 2,
+
+        [Description("Restarted")]
+        restarted = 3,
+
+        [Description("Finished")]
+        finished = 4
+    }
+}
